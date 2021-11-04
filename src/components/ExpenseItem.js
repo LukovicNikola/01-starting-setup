cda70d355059dd5f239e6e0b862b5c9574a1c795
LukovicNikola <lukovic89@gmail.com>
2021-11-04 22:22:01 +0100

    first

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div>March 28th 2021</div>
+        <div><h2>Car Insurance</h2></div>
+        <div>$294.55</div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
