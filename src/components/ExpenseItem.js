f6b293e7d00c1d79de4bf1e23b7d293e518ba980
LukovicNikola <lukovic89@gmail.com>
2021-11-04 23:10:07 +0100

    adding expenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDateDay = new Date().getDay();
+    const expenseDateMonth = new Date().getMonth();
+    const expenseDateYear = new Date().getFullYear();
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 255.55;
+
     return (
-    <div>
-        <div>March 28th 2021</div>
-        <div><h2>Car Insurance</h2></div>
-        <div>$294.55</div>
-    </div>
+        <div className="expense-item">
+            <div>{expenseDateDay}.{expenseDateMonth}.{expenseDateYear}</div>
+            <div className="expense-item__description">
+                <h2 className="expense-item h2">{expenseTitle}</h2>
+            </div>
+            <div className="expense-item__price">${expenseAmount}</div>
+        </div>
     );
 }
 
