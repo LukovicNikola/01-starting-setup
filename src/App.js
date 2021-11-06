4e45fa3874e22183f86b702bc521dd23d7e00b6a
LukovicNikola <lukovic89@gmail.com>
2021-11-06 17:44:18 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -22,8 +23,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  function addExpenseHandler(expense) {
+    console.log(expense);
+  }
+
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
