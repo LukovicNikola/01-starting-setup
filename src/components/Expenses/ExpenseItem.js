36e70e251f97b0a41a0333a05955cfe99402af50
LukovicNikola <lukovic89@gmail.com>
2021-11-06 23:34:20 +0100

    commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  function clickHandler() {
-    setTitle("promenjen");
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2 className="expense-item h2">{title}</h2>
+        <h2 className="expense-item h2">{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>Edit Title</button>
     </Card>
   );
 }
